4cc096bed4422b20a6ae7858fa9dc78effb29731
Josemar Magalhaes <j.magalhaes@mytaxi.com>
2019-06-27 17:04:39 +0200

    voyager

--- src/app/main.go
+++ src/app/main.go
@@ -6,7 +6,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Hello From the Delta Quadrant",
+			"message": "Voyager: Hello From the Delta Quadrant",
 		})
 	})
 
